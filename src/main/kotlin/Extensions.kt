1a791dae360f86d71a8e2871f61aba384b45ed1e
Stian Østerhaug <stian.osterhaug@gmail.com>
2018-03-13 00:03:08 +0100

    Config.getLong

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -2,8 +2,10 @@ package org.pyrhus.config
 
 fun Property.asString() = this.value
 fun Property.asInt() = this.value.toInt()
+fun Property.asLong() = this.value.toLong()
 fun Property.asBoolean() = this.value.toBoolean()
 
 fun Config.getString(key: String) = this[key].asString()
 fun Config.getInt(key: String) = this[key].asInt()
+fun Config.getLong(key: String) = this[key].asLong()
 fun Config.getBoolean(key: String) = this[key].asBoolean()
