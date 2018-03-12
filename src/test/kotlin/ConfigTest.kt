a8989ce236214ac5d056549895c6deab0400c5a7
Stian Østerhaug <stian.osterhaug@gmail.com>
2018-03-12 17:52:32 +0100

    test typesafe pattern

--- src/test/kotlin/ConfigTest.kt
+++ src/test/kotlin/ConfigTest.kt
@@ -18,4 +18,23 @@ class ConfigTest {
             """.trimIndent()
         assertThat(config.toString()).isEqualTo(expected)
     }
+
+    @Test
+    fun testTypesafe() {
+        val config = loadConfig {
+            add(property("message", "hello", true))
+            add(property("number", "3"))
+            add(property("boolean", "true"))
+        }
+        val myConfig = MyConfig(config)
+        assertThat(myConfig.message).isEqualTo("hello")
+        assertThat(myConfig.boolean).isEqualTo(true)
+        assertThat(myConfig.number).isEqualTo(3)
+    }
+}
+
+private class MyConfig(config: Config) {
+    val message = config.getString("message")
+    val boolean = config.getBoolean("boolean")
+    val number = config.getInt("number")
 }
