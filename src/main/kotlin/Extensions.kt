47bff50470cd25eb5f467cdf6f9b66c246b3682c
Stian Østerhaug <stian.osterhaug@gmail.com>
2018-06-07 09:45:43 +0200

    add getIntOrNull/getLongOrNull/getBooleanOrNull

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -6,7 +6,10 @@ fun Property.asLong() = this.asString().toLong()
 fun Property.asBoolean() = this.asString().toBoolean()
 
 fun Config.getString(key: String) = this[key].asString()
-fun Config.getStringOrNull(key: String): String? = this.getOrNull(key)?.value
+fun Config.getStringOrNull(key: String) = this.getOrNull(key)?.value
 fun Config.getInt(key: String) = this[key].asInt()
+fun Config.getIntOrNull(key: String) = this.getOrNull(key)?.asInt()
 fun Config.getLong(key: String) = this[key].asLong()
+fun Config.getLongOrNull(key: String) = this.getOrNull(key)?.asLong()
 fun Config.getBoolean(key: String) = this[key].asBoolean()
+fun Config.getBooleanOrNull(key: String) = this.getOrNull(key)?.asBoolean()
